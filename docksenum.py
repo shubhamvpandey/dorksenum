f3c094b25631cf76efc091036d1526f0550b89db
shubhamvpandey <77104060+shubhamvpandey@users.noreply.github.com>
2023-06-10 18:08:19 +0530

    Create docksenum.py

---
+++ docksenum.py
@@ -0,0 +1,107 @@
+import webbrowser
+import requests
+from bs4 import BeautifulSoup
+
+# Function to perform the Google search and display the results
+def run_dork(keyword, dork):
+    query = get_query(keyword, dork)
+
+    print(f"\n----- Dork {dork} -----\n")
+    print(f"Performing Google search for '{query}'...")
+
+    search_results = []
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(f"https://www.google.com/search?q={query}", headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    results = soup.select('a')
+    
+    for result in results:
+        url = result.get('href')
+        if url.startswith('/url?q='):
+            url = url[7:]
+            if 'google' not in url:
+                search_results.append(url)
+
+    if search_results:
+        print("\n----- Search Results -----")
+        for result in search_results:
+            print(result)
+    else:
+        print("No results found.")
+
+# Function to get the dork query based on the dork number
+def get_query(keyword, dork):
+    dork_queries = {
+        1: f"site:github.com OR site:gitlab.com OR site:bitbucket.org {keyword}",
+        2: f"site:s3.amazonaws.com {keyword}",
+        3: f"site:{keyword} -www {keyword}",
+        4: f"site:pastebin.com {keyword}",
+        5: f"site:{keyword} intitle:phpinfo",
+        6: f"site:{keyword} intitle:index of OR inurl:/logs",
+        7: f"site:{keyword} intext:'SQL Error'",
+        8: f"site:{keyword} intitle:apache +intext:conf | cnf | config",
+        9: f"site:{keyword} intitle:index of OR inurl:/",
+        10: f"site:{keyword} ext:sql OR ext:dbf OR ext:mdb",
+        11: f"site:{keyword} ext:doc OR ext:docx OR ext:odt OR ext:pdf OR ext:rtf OR ext:sxw OR ext:psw OR ext:ppt OR ext:pptx OR ext:pps OR ext:csv",
+        12: f"site:{keyword} inurl:readme OR inurl:license OR inurl:install OR inurl:setup OR inurl:config",
+        13: f"site:{keyword} inurl:/phpinfo.php OR inurl:.htaccess",
+        14: f"site:atlassian.net OR site:bitbucket.org {keyword}"
+    }
+    return dork_queries.get(dork, "")
+
+# Function to display the help menu
+def display_help():
+    print("Usage: python dork_search.py")
+    print("Interactive Dork Search Menu:")
+    print("1. GitHub, GitLab, Bitbucket Dork")
+    print("2. S3 Bucket Dork")
+    print("3. Subdomain Dork")
+    print("4. Pastebin Dork")
+    print("5. intitle:phpinfo Dork")
+    print("6. Logs Dork")
+    print("7. SQL Errors Dork")
+    print("8. Apache Config Files Dork")
+    print("9. Index and Directory Dork")
+    print("10. Database Files Dork")
+    print("11. File Extensions Dork")
+    print("12. Specific Files in URLs Dork")
+    print("13. PHP Info and .htaccess Files Dork")
+    print("14. Atlassian and Bitbucket Dork")
+    print("-h or --help: Display this help menu")
+
+# Interactive menu
+print("Welcome to Dork Search!")
+print("Please provide a keyword or domain name to search for.")
+keyword = input("Keyword or domain name: ")
+
+print("\nAvailable dorks:")
+print("1. GitHub, GitLab, Bitbucket Dork")
+print("2. S3 Bucket Dork")
+print("3. Subdomain Dork")
+print("4. Pastebin Dork")
+print("5. intitle:phpinfo Dork")
+print("6. Logs Dork")
+print("7. SQL Errors Dork")
+print("8. Apache Config Files Dork")
+print("9. Index and Directory Dork")
+print("10. Database Files Dork")
+print("11. File Extensions Dork")
+print("12. Specific Files in URLs Dork")
+print("13. PHP Info and .htaccess Files Dork")
+print("14. Atlassian and Bitbucket Dork")
+print("Enter 'all' to run all dorks.")
+
+dork_input = input("Select a dork number or enter 'all': ")
+
+if dork_input == "all":
+    for i in range(1, 15):
+        run_dork(keyword, i)
+else:
+    try:
+        dork_number = int(dork_input)
+        run_dork(keyword, dork_number)
+    except ValueError:
+        print("Invalid input. Please select a valid dork number.")
+
+print("\nThank you for using Dork Search!")
+
